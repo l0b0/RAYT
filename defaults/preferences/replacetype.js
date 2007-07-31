84c79c4fecaf76d657ebc29e89f9c30e12dbe283
Victor Engmark <victor.engmark@gmail.com>
2007-07-31 12:17:09 +0000

    Firefox extension. Mostly auto-generated content so far.

---
+++ defaults/preferences/replacetype.js
@@ -0,0 +1,6 @@
+pref("extensions.replacetype.boolpref", false);
+pref("extensions.replacetype.intpref", 0);
+pref("extensions.replacetype.stringpref", "A string");
+pref("extensions.replacetype.boolpref", false);
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.ret@l0b0.net.description", "chrome://replacetype/locale/replacetype.properties");
