ef10430f9bac9910ef1f6cb2185611eaea5f9bf8
Victor Engmark <victor.engmark@gmail.com>
2007-09-07 12:04:57 +0000

    Added the current preferences.

--- defaults/preferences/rayt.js
+++ defaults/preferences/rayt.js
@@ -1,6 +1,12 @@
-pref("extensions.rayt.boolpref", false);
-pref("extensions.rayt.intpref", 0);
-pref("extensions.rayt.stringpref", "A string");
-pref("extensions.rayt.boolpref", false);
+//pref("extensions.rayt.boolpref", false);
+//pref("extensions.rayt.intpref", 0);
+//pref("extensions.rayt.stringpref", "A string");
+pref("extensions.rayt.double_primes_style", "quotes");
+pref("extensions.rayt.single_primes_style", "quotes");
+pref("extensions.rayt.replace_prime_in_word", true);
+pref("extensions.rayt.replace_ellipsis", true);
+pref("extensions.rayt.replace_numero", false);
+
+
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.ret@l0b0.net.description", "chrome://rayt/locale/rayt.properties");
