77ba2e421e69561687f1a11528cec5479c115622
Victor Engmark <victor.engmark@gmail.com>
2007-09-07 13:06:48 +0000

    Renamed preferences to reflect the matches, not the result. Corrected description target.

--- defaults/preferences/rayt.js
+++ defaults/preferences/rayt.js
@@ -1,12 +1,9 @@
-//pref("extensions.rayt.boolpref", false);
 //pref("extensions.rayt.intpref", 0);
-//pref("extensions.rayt.stringpref", "A string");
 pref("extensions.rayt.double_primes_style", "quotes");
 pref("extensions.rayt.single_primes_style", "quotes");
 pref("extensions.rayt.replace_prime_in_word", true);
-pref("extensions.rayt.replace_ellipsis", true);
-pref("extensions.rayt.replace_numero", false);
-
+pref("extensions.rayt.replace_triple_dots", true);
+pref("extensions.rayt.replace_no", false);
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
-pref("extensions.ret@l0b0.net.description", "chrome://rayt/locale/rayt.properties");
+pref("extensions.rayt@l0b0.net.description", "chrome://rayt/locale/rayt.properties");
